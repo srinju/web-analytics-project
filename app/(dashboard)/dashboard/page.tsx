537f5187e56bb15c7270fd782d5d924a4fab2105
SRINJOY DAS <dassrinjoy333@gmail.com>
2024-11-12 03:05:39 +0530

    working on the dashboard

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+const dashboard = () => {
+    return (
+        <div>
+            dashboard
+        </div>
+    )
+}
+
+export default dashboard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './lib/auth';
 
-export default function Home() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const session = await getServerSession(authOptions);
+      if (session?.user) {
+        router.push('/dashboard');
+      } else {
+        router.push('/api/auth/signin');
+      }
+    };
+    
+    checkSession();
+  }, [router]);
+
+  return <div>Loading...</div>; // You can show a loading spinner or message here
 }
